e19c6149e16cfad96f9cf0b8bda56be322d56c7c
Pietromuresu <muresupietro66@gmail.com>
2023-06-01 15:21:21 +0200

    Add name and surname dynamically

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $name= 'Pietro';
+    $surname= 'Muresu';
+    return view('home', compact('name', 'surname'));
 })->name('home');
